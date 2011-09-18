2d718882852e5ff7cccc88a945d95220d71b6b95
Bruno Grasselli <bruno.grasselli@gmail.com>
2011-09-18 12:22:00 -0300

    focus when page is ready

--- assets/javascripts/calculator_initializer.js
+++ assets/javascripts/calculator_initializer.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
   $("#arrived_at").val(window.localStorage.time);
   var calculator = new Calculator();
   new CalculatorController(calculator);
+  $("#arrived_at").focus();
 });
 
